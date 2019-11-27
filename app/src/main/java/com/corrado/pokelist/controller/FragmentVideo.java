ef9b187cfef3984e407bc94417e39afd54fb8c36
corradocalvi <calvi@et.esiea.fr>
2019-11-27 16:24:19 +0100

    Addind Fragment

---
+++ app/src/main/java/com/corrado/pokelist/controller/FragmentVideo.java
@@ -0,0 +1,26 @@
+package com.corrado.pokelist.controller;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.corrado.pokelist.R;
+
+public class FragmentVideo extends Fragment {
+    public View v;
+
+    public FragmentVideo() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v=inflater.inflate(R.layout.video_fragment, container, false);
+        return v;
+    }
+}
